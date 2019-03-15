7b8ad5ffc4c02cdd152bf5de6ab42dc4373d6e87
GLYASAI <chaoschunqfeijg@163.com>
2019-03-15 16:52:00 +0800

    first commit

---
+++ src/main/java/org/rainbond/testinitprobe/TestinitprobeApplication.java
@@ -0,0 +1,13 @@
+package org.rainbond.testinitprobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestinitprobeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestinitprobeApplication.class, args);
+    }
+
+}
