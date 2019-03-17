f56df7a9885906c1d910ef91e184b77af4cf9a9e
GLYASAI <chaoschunqfeijg@163.com>
2019-03-17 14:50:55 +0800

    [ADD] gs-accessing-data-mysql

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,13 +1,12 @@
-package org.rainbond.testinitprobe;
+package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TestinitprobeApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(TestinitprobeApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
-
 }
